f75ef9626e04beac70edd66a477c5d3228e01a69
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-10-11 19:59:39 -0600

    First running web platform

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 
 
 from fastapi import FastAPI
-# from typing import Optional
+from typing import Optional
 
 app = FastAPI()
 
@@ -12,7 +12,8 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
 
-# @app.get("/items/{item_id}")
-# def read_item(item_id: int, q: Union[str, None] = None):
-#     return {"item_id": item_id, "q": q}
+
